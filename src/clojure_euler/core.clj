a0754cd7f71177df5a9a9c59aab986a01440476f
Javier Holguera <javier.holguera@fundingcircle.com>
2016-08-26 17:49:10 +0100

    Solutions for problem 1 and 2

--- src/clojure_euler/core.clj
+++ src/clojure_euler/core.clj
@@ -1,6 +1,18 @@
 (ns clojure-euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn problem1 []
+  (letfn [(multiple-fn ([n] (or
+                             (zero? (mod n 3))
+                             (zero? (mod n 5)))))]
+    (->> (range 0 1000)
+         (filter multiple-fn)
+         (reduce +))))
+
+(defn problem2 []
+  (letfn [(fibo-gen ([] (map first
+                             (iterate (fn [[a b]] [b (+ a b)])
+                                      [1 2]))))]
+    (->> (fibo-gen)
+         (take-while (fn [n] (< n 4000000)))
+         (filter even?)
+         (reduce +))))
